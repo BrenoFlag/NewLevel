9ff8ce6ea4c887dcd707950e7f4ff0ee84728069
BrenoFlag <117334417+BrenoFlag@users.noreply.github.com>
2025-10-22 17:00:16 -0400

    Use session theme to persist mode across pages

--- src/assets/js/dark.js
+++ src/assets/js/dark.js
@@ -5,17 +5,21 @@
 // helper functions to toggle dark mode
 function enableDarkMode() {
     document.body.classList.add("dark-mode");
-    localStorage.setItem("theme", "dark");
+    sessionStorage.setItem("theme", "dark");
 }
 function disableDarkMode() {
     document.body.classList.remove("dark-mode");
-    localStorage.setItem("theme", "light");
+    sessionStorage.setItem("theme", "light");
 }
 
 // determines a new users dark mode preferences
 function detectColorScheme() {
-    // always start the site in light mode regardless of saved or system preferences
-    disableDarkMode();
+    // default to light mode, but respect the theme chosen during this browser session
+    if (sessionStorage.getItem("theme") === "dark") {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
 }
 
 // run on page load
@@ -23,6 +27,6 @@ detectColorScheme();
 
 // add event listener to the dark mode button toggle
 document.getElementById("dark-mode-toggle").addEventListener("click", () => {
-    // on click, check localStorage for the dark mode value, use to apply the opposite of what's saved
-    localStorage.getItem("theme") === "light" ? enableDarkMode() : disableDarkMode();
+    // on click, check the current session's dark mode value, use to apply the opposite of what's saved
+    sessionStorage.getItem("theme") === "light" ? enableDarkMode() : disableDarkMode();
 });
